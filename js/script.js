48f8aa98c00811617bc74995cab7e81934d0f118
habudala <habudala@live.com>
2016-03-28 10:53:37 -0400

    Angular JS practice
    Introduction of Angular JS, and practice.

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// var myInput= document.getElementById("myinput");
+
+// myInput.addEventListener("keyup", function(){
+// 	// console.log("User hit a key");
+
+// 	var userText = myInput.value;
+// 	document.getElementById("inputText").innerHTML = userText;
+// })
+// //alternative to onclick in html
+
+
+
+// $("#myinput").keyup(function(){
+// 	$("#myinput").html($("#myinput").val());
+// });
+// piece of shit doesn't work in Jquery
